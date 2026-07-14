51db3e2a54b58dd163cda24a73b4dea3ad8e772e
sumedhacp <sumedhastudy2025@gmail.com>
2026-07-15 01:37:03 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddVehicle from './components/AddVehicle'
+import ViewVehicle from './components/ViewVehicle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
- 
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path='/' element={<AddVehicle />} />
+          <Route path='/view' element={<ViewVehicle />} />
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
